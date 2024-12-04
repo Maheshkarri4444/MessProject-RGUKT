7d1fa55b2c6cba0f364afa18a67a060fe82fae24
Maheshkarri4444 <maheshkarri2109@gmail.com>
2024-12-04 22:05:14 +0530

    fixed the error

--- backend/routes/messauthority.routes.js
+++ backend/routes/messauthority.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import getFeedback from "../controllers/feedback.controller.js"
+import {getFeedback} from "../controllers/feedback.controller.js"
 import protectRoute_authority from "../middleware/protectRouteAuthority.js";
 import { getDailyComplaints,getWeeklyComplaints } from "../controllers/complaint.controller.js";
 import { getAllIssues , updateSingleIssue ,deleteSingleIssue } from "../controllers/issue.controller.js";
 
 const router = express.Router();
 
-router.get("/feedback/:feedback_type/:messId",protectRoute_authority, getFeedback);//ignore this for a while until i clear the error
+router.get("/feedback/:feedback_type/:messId",protectRoute_authority, getFeedback);
 router.get("/complaints/daily/:messId",protectRoute_authority,getDailyComplaints);
 router.get("/complaints/weekly/:messId",protectRoute_authority,getWeeklyComplaints);
 router.get("/issues",protectRoute_authority,getAllIssues);
